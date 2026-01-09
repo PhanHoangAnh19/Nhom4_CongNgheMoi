379947f38a4c1193b75ab2535b6efeb68d5be075
AnhPhan <angphan5@gmail.com>
2026-01-09 20:59:43 +0700

    hiển thị tên đăng nhập trên trang web shop

---
+++ storage/framework/views/a8d1092222c7bf3bef299d6a1aee8f0f.php
@@ -0,0 +1,4 @@
+<?php $__env->startSection('content'); ?>
+    <h1>Chào mừng đến với cửa hàng điện thoại</h1>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.client', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\xampp\htdocs\Nhom4_CongNgheMoi\resources\views/client/index.blade.php ENDPATH**/ ?>
